a5b83e0317e607e7bbcf6025bce2e5029e1219c7
Santo Commarato <cylynex@gmail.com>
2021-08-30 10:10:36 -0600

    Initial commit

---
+++ Assets/Scripts/BreakGlass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakGlass : MonoBehaviour {
+
+    [SerializeField] GameObject brokenGlass;
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] AudioSource audio;
+
+    private void OnMouseDown() {
+        GameObject bg = Instantiate(brokenGlass, transform.position, transform.rotation);
+        audio.Play();
+        Destroy(gameObject);
+    }
+
+}
